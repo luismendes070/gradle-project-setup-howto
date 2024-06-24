c3d5dc002cd825c32f9db4a3aee584525f493a83
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-06-24 14:05:25 +0200

    Add Convention Plugins for Java Modules

--- gradle/plugins/src/main/kotlin/org.example.gradle.component.library.gradle.kts
+++ gradle/plugins/src/main/kotlin/org.example.gradle.component.library.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.gradle.java-library")
     id("org.example.gradle.base.dependency-rules")
+    id("org.example.gradle.base.module-system-rules")
     id("org.example.gradle.base.identity")
     id("org.example.gradle.base.lifecycle")
     id("org.example.gradle.check.dependencies")
